8365d1c3b8d434541949fb8cc6ca21adcf501763
aquilesics <aquiles.cout@gmail.com>
2023-06-15 19:00:11 -0300

    logic gates prediction

--- gates.c
+++ gates.c
@@ -1,15 +1,20 @@
+#include <math.h>
 #include <stdatomic.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+float sigmoidf(float x){
+    return 1.f/ (1.f + expf(-x));
+}
+
 //OR-gate
 float train[][3] =
  {
        {0,0,0},
-       {1,0,1},
-       {0,1,1},
+       {1,0,0},
+       {0,1,0},
        {1,1,1}
     };
 #define train_count ( sizeof(train)/sizeof(train[0]) )
@@ -18,13 +23,13 @@ float  rand_float(void){
     return (float) rand()/ (float) RAND_MAX; 
 };
 
-float cost(float w1,  float w2){
+float cost(float w1,  float w2, float b){
     float result = 0.0f;
     for (size_t i = 0; i < train_count; ++i ){
         float x1 = train[i][0];
         float x2 = train[i][1];
 
-        float y = x1*w1 + x2*w2;
+        float y = sigmoidf( x1*w1 + x2*w2 +b);
         float dist = y - train[i][2]; //distance
         result += dist*dist;
         // printf("actual: %f; expected: %"d"f\n",y,train[i][1]);
@@ -37,20 +42,31 @@ int main(void){
     srand(69);
     float w1 = rand_float();
     float w2 = rand_float();
-    float eps = 1e-3;
-    float rate = 1e-3;
+    float b = rand_float();
 
-    for( size_t i = 0; i < 5050; ++i ){
-        float c = cost(w1,  w2);
-        printf("w1 = %f , w2 = %f\n",w1,w2);
+    float eps = 1e-1;
+    float rate = 1e-1;
 
-        float d1 = (cost(w1 + eps, w2) - c)/eps;
-        float d2 = (cost(w1, w2 + eps) - c)/eps;
+    for( size_t i = 0; i < 5000; ++i ){
+        float c = cost(w1,  w2,b);
+        printf("w1=%f, w2=%f,b=%f, c=%f\n", w1, w2, b, c );
 
+        float d1 = (cost(w1 + eps, w2,b) - c)/eps;
+        float d2 = (cost(w1, w2 + eps,b) - c)/eps;
+        float db = (cost(w1, w2,b + eps) - c)/eps;
+;
         w1 -= rate*d1;
         w2 -= rate*d2;
+        b -= rate*db;
 
         printf("%f\n",c);
+        
+    }
+    printf("_____________\n");
+    for(size_t i =0; i < 2;++i){
+        for (size_t j =0; j < 2;++j) {
+            printf("%zu | %zu = %f\n",i,j,sigmoidf(i*w1 +j*w2 +b) );        
+        }
     }
     
     return 0;
